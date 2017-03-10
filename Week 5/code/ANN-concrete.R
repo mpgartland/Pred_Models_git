c2dd15c02ad8c902196f3c1c48a064b8c15a6b91
mpgartland <myles.gartland@rockhurst.edu>
2017-03-10 15:10:23 -0600

    March_10

---
+++ Week 5/code/ANN-concrete.R
@@ -0,0 +1,103 @@
+
+```{r set2}
+setwd('~/Documents/Courses/Predictive Models/PM/Week 6/')
+## Step 2: Exploring and preparing the data ----
+# read in data and examine structure
+concrete <- read.csv("concrete.csv")
+str(concrete)
+boxplot(concrete) #see the distibutions of each variable
+````
+
+
+#custom normalization function
+#This is called min/max normalization (vs z-score)
+#Normalization by Scaling Between 0 and 1 
+#Common way for ANN
+normalize <- function(x) { 
+  return((x - min(x)) / (max(x) - min(x)))
+}
+
+# apply min/max normalization to entire data frame
+#note all values are now between 0 and 1
+concrete_norm <- as.data.frame(lapply(concrete, normalize))
+boxplot(concrete_norm)
+# confirm that the range is now between zero and one
+summary(concrete_norm$strength)
+
+# compared to the original minimum and maximum
+summary(concrete$strength)
+
+# create training and test data 
+#Split the dataset into a training and testing sets 70/30
+concrete_train <- concrete_norm[1:773, ]
+concrete_test <- concrete_norm[774:1030, ]
+
+## Step 3: Training a model on the data ----
+# train the neuralnet model
+library(neuralnet)
+
+# simple ANN with only a two hidden neurons
+concrete_model_2 <- neuralnet(formula = strength ~ cement + slag +
+                              ash + water + superplastic + 
+                              coarseagg + fineagg + age,
+                            data = concrete_train, hidden = 2, algorithm = "rprop+", learningrate=NULL)
+#rprop+ is a backpropagation method called resilient backpropagation. It modifies
+#its learning rate on the error.
+
+
+# visualize the network topology
+#note one node in the hidden layer
+plot(concrete_model_2)
+
+#table of nuerons and weights
+concrete_model_2$result.matrix 
+
+## Step 4: Evaluating model performance ----
+# obtain model results
+model_results_2 <- compute(concrete_model_2, concrete_test[1:8]) #You are running the training set through the ANN model
+# obtain predicted strength values
+predicted_strength_2 <- model_results_2$net.result #The prediction of each observation
+# examine the correlation between predicted and actual values
+cor(predicted_strength_2, concrete_test$strength)
+
+#RMSE
+sqrt(mean((concrete_test$strength-predicted_strength_2)^2))
+
+## Step 5: Improving model performance ----
+# a more complex neural network topology with 5 hidden neurons
+concrete_model2 <- neuralnet(strength ~ cement + slag +
+                               ash + water + superplastic + 
+                               coarseagg + fineagg + age,
+                             data = concrete_train, hidden = 5,algorithm = "rprop+", learningrate=NULL)
+
+# plot the network
+#note 5 neurons in the hidden layer
+plot(concrete_model2)
+
+# evaluate the results as we did before
+model_results2 <- compute(concrete_model2, concrete_test[1:8])
+predicted_strength2 <- model_results2$net.result
+cor(predicted_strength2, concrete_test$strength)
+
+predicted_strength2[1:10]
+#what do you notice about the values?
+
+#Return norm value to a regular value
+denormalize <- function(x) { 
+  return(x*(max(concrete$strength)) - min(concrete$strength))+min(concrete$strength)
+}
+
+
+#look at predicted vs actual
+accuracy<-data.frame(denormalize(predicted_strength2),concrete$strength[774:1030])
+
+#plot pred vs actual
+plot(denormalize(predicted_strength2),concrete$strength[774:1030])
+
+#Model with two hidden layers
+concrete_model3 <- neuralnet(strength ~ cement + slag +
+                               ash + water + superplastic + 
+                               coarseagg + fineagg + age,
+                             data = concrete_train, hidden = c(5,3), algorithm = "rprop+", learningrate=NULL)
+
+plot(concrete_model3)
---
+++ Week 5/code/ANN-credit.R
@@ -0,0 +1,102 @@
+setwd('/Users/mylesgartland/Documents/Courses/Predictive Models/Week 5')
+library("neuralnet")
+# you can also see the packages nnet and RSNNS
+
+dataset <- read.csv("creditset.csv")
+head(dataset)
+
+## extract a set to train the NN @ 65%
+trainset <- dataset[1:1300, ]
+
+## select the test set
+testset <- dataset[1301:2000, ]
+
+#NN nodel with one hiddeD layer and 4 nodes, resilient backprop
+creditnet <- neuralnet(default10yr ~ LTI +age, trainset, hidden = 4, lifesign = "minimal", 
+                       linear.output = FALSE, threshold = 0.1,algorithm = "rprop+")
+
+
+
+print(creditnet)
+
+## plot the NN
+plot(creditnet, rep = "best")
+
+#weight from models
+creditnet$weights
+
+creditnet$result.matrix
+
+#results (notice in probabilities)
+creditnet$net.result
+
+
+## test the resulting output
+temp_test <- subset(testset, select = c("LTI", "age"))
+
+#compute the test results through the ANN model 
+creditnet.results <- compute(creditnet, temp_test)
+
+#Make a table to predictions and actuals
+results <- data.frame(actual = testset$default10yr, 
+                      prediction = creditnet.results$net.result)
+
+print(results)
+
+#Round predictions to make easier to read
+results$prediction <- round(results$prediction)
+
+#See partial results
+results[100:115, ]
+
+#See all results
+print(results)
+
+print(creditnet.results)
+
+
+
+#USing Gmodels to create a confusion matrix
+library(gmodels)
+CrossTable(results$actual, results$prediction)
+
+
+################################################
+#Arguments in neural net function
+  neuralnet(formula, data, hidden = 1, threshold = 0.01,        
+            stepmax = 1e+05, rep = 1, startweights = NULL, 
+            learningrate.limit = NULL, 
+            learningrate.factor = list(minus = 0.5, plus = 1.2), 
+            learningrate=NULL, lifesign = "none", 
+            lifesign.step = 1000, algorithm = "rprop+", 
+            err.fct = "sse", act.fct = "logistic", 
+            linear.output = TRUE, exclude = NULL, 
+            constant.weights = NULL, likelihood = FALSE)
+#######################################################
+
+
+#two hidden layers of 2 and 4 nodes. 
+
+creditnet1 <- neuralnet(default10yr ~ LTI +age, trainset, hidden=c(2,4), lifesign = "minimal", 
+                       linear.output = FALSE, threshold = 0.1,algorithm = "rprop+")
+
+
+
+print(creditnet1)
+
+plot(creditnet1)
+
+###Try happends are you add/less more hidden node, more variables, and changes in arguments
+
+
+#In neural networks it is good idea not just normalize data, but also to scale them. 
+#This is intended for faster approaching to global minima at error surface. 
+#Common way for ANN
+normalize <- function(x) { 
+  return((x - min(x)) / (max(x) - min(x)))
+}
+
+# apply min/max normalization to entire data frame
+#note all values are now between 0 and 1
+creditset_norm <- as.data.frame(lapply(creditset, normalize))
+
